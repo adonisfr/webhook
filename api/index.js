0957eb3e075920a03c49cb44ce0155e2bc6e1cac
A2nis <adonisfernandezr@gmail.com>
2023-01-27 09:21:13 -0500

    updated /api/webhook

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const request = require("request"),
 app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
 
 // Accepts POST requests at /webhook endpoint
-app.post("/webhook", (req, res) => {
+app.post("/api/webhook", (req, res) => {
   // Parse the request body from the POST
   let body = req.body;
 
